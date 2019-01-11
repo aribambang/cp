8a229503ccdeb4ad0693ce7fe274b0b1ace324f3
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-01-12 00:43:41 +0700

    init

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define bambang using
+#define pengen namespace
+#define lulus std
+
+bambang pengen lulus;
+
+typedef long l;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<l> vl;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<l,l> pl;
+typedef pair<ll, ll> pll;
+typedef pair<ld,ld> pld;
+
+#define nl printf("\n");
+#define sp printf(" ");
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+
+#define fi first
+#define se second
+
+#define scc(x) scanf("%c", &x)
+#define scd(x) scanf("%Lf", &x)
+#define scf(x) scanf("%f", &x)
+#define sci(x) scanf("%d", &x)
+#define scl(x) scanf("%ld", &x)
+#define scll(x) scanf("%lld", &x)
+#define scld(x) cin >> x;
+#define scui(x) sci(x)
+#define scul(x) scl(x)
+#define scull(x) scll(x)
+
+
+int main(){
+
+}
